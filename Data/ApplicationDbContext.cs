d7f722afb642c023010ec2e0b33b20604503eb66
Lee-Rhaan <rhaanking@gmail.com>
2021-08-02 21:23:01 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BooksWebApp.Models;
+
+namespace BooksWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BooksWebApp.Models.Book> Book { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksWebApp.Models
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string BookText { get; set; }
+
+        public Book()
+        {
+
+        }
+    }
+}
